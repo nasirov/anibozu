5f8ad3792f4fb56f18eb52edc9cc1f57f666b7b6
hikka <yurqaa@mail.ru>
2019-01-30 12:04:21 +0300

    refactor AnimediaMALTitleReferences

--- src/main/java/nasirov/yv/serialization/AnimediaMALTitleReferences.java
+++ src/main/java/nasirov/yv/serialization/AnimediaMALTitleReferences.java
@@ -1,5 +1,8 @@
 package nasirov.yv.serialization;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 /**
@@ -7,6 +10,9 @@ import org.codehaus.jackson.annotate.JsonIgnoreProperties;
  * Created by Хикка on 29.12.2018.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnimediaMALTitleReferences {
 	/**
 	 * URL on Animedia
@@ -58,22 +64,6 @@ public class AnimediaMALTitleReferences {
 	 */
 	private String numberOfEpisodeForWatch;
 	
-	public AnimediaMALTitleReferences() {
-	}
-	
-	public AnimediaMALTitleReferences(String url, String dataList, String firstEpisode, String titleOnMAL, String min, String max, String currentMax, String posterUrl, String finalUrl, String numberOfEpisodeForWatch) {
-		this.url = url;
-		this.dataList = dataList;
-		this.firstEpisode = firstEpisode;
-		this.titleOnMAL = titleOnMAL;
-		this.min = min;
-		this.max = max;
-		this.currentMax = currentMax;
-		this.posterUrl = posterUrl;
-		this.finalUrl = finalUrl;
-		this.numberOfEpisodeForWatch = numberOfEpisodeForWatch;
-	}
-	
 	public AnimediaMALTitleReferences(AnimediaMALTitleReferences animediaMALTitleReference) {
 		this.url = animediaMALTitleReference.getUrl();
 		this.dataList = animediaMALTitleReference.getDataList();
@@ -85,133 +75,5 @@ public class AnimediaMALTitleReferences {
 		this.posterUrl = animediaMALTitleReference.getPosterUrl();
 		this.finalUrl = animediaMALTitleReference.getFinalUrl();
 		this.numberOfEpisodeForWatch = animediaMALTitleReference.getNumberOfEpisodeForWatch();
-	}
-	
-	public String getUrl() {
-		return url;
-	}
-	
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	
-	public String getDataList() {
-		return dataList;
-	}
-	
-	public void setDataList(String dataList) {
-		this.dataList = dataList;
-	}
-	
-	public String getFirstEpisode() {
-		return firstEpisode;
-	}
-	
-	public void setFirstEpisode(String firstEpisode) {
-		this.firstEpisode = firstEpisode;
-	}
-	
-	public String getTitleOnMAL() {
-		return titleOnMAL;
-	}
-	
-	public void setTitleOnMAL(String titleOnMAL) {
-		this.titleOnMAL = titleOnMAL;
-	}
-	
-	public String getMin() {
-		return min;
-	}
-	
-	public void setMin(String min) {
-		this.min = min;
-	}
-	
-	public String getMax() {
-		return max;
-	}
-	
-	public void setMax(String max) {
-		this.max = max;
-	}
-	
-	public String getCurrentMax() {
-		return currentMax;
-	}
-	
-	public void setCurrentMax(String currentMax) {
-		this.currentMax = currentMax;
-	}
-	
-	public String getPosterUrl() {
-		return posterUrl;
-	}
-	
-	public void setPosterUrl(String posterUrl) {
-		this.posterUrl = posterUrl;
-	}
-	
-	public String getFinalUrl() {
-		return finalUrl;
-	}
-	
-	public void setFinalUrl(String finalUrl) {
-		this.finalUrl = finalUrl;
-	}
-	
-	public String getNumberOfEpisodeForWatch() {
-		return numberOfEpisodeForWatch;
-	}
-	
-	public void setNumberOfEpisodeForWatch(String numberOfEpisodeForWatch) {
-		this.numberOfEpisodeForWatch = numberOfEpisodeForWatch;
-	}
-	
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		AnimediaMALTitleReferences that = (AnimediaMALTitleReferences) o;
-		if (!url.equals(that.url)) return false;
-		if (!dataList.equals(that.dataList)) return false;
-		if (!firstEpisode.equals(that.firstEpisode)) return false;
-		if (!titleOnMAL.equals(that.titleOnMAL)) return false;
-		if (min != null ? !min.equals(that.min) : that.min != null) return false;
-		if (max != null ? !max.equals(that.max) : that.max != null) return false;
-		if (currentMax != null ? !currentMax.equals(that.currentMax) : that.currentMax != null) return false;
-		if (posterUrl != null ? !posterUrl.equals(that.posterUrl) : that.posterUrl != null) return false;
-		if (finalUrl != null ? !finalUrl.equals(that.finalUrl) : that.finalUrl != null) return false;
-		return numberOfEpisodeForWatch != null ? numberOfEpisodeForWatch.equals(that.numberOfEpisodeForWatch) : that.numberOfEpisodeForWatch == null;
-	}
-	
-	@Override
-	public int hashCode() {
-		int result = url.hashCode();
-		result = 31 * result + dataList.hashCode();
-		result = 31 * result + firstEpisode.hashCode();
-		result = 31 * result + titleOnMAL.hashCode();
-		result = 31 * result + (min != null ? min.hashCode() : 0);
-		result = 31 * result + (max != null ? max.hashCode() : 0);
-		result = 31 * result + (currentMax != null ? currentMax.hashCode() : 0);
-		result = 31 * result + (posterUrl != null ? posterUrl.hashCode() : 0);
-		result = 31 * result + (finalUrl != null ? finalUrl.hashCode() : 0);
-		result = 31 * result + (numberOfEpisodeForWatch != null ? numberOfEpisodeForWatch.hashCode() : 0);
-		return result;
-	}
-	
-	@Override
-	public String toString() {
-		return "AnimediaMALTitleReferences{" +
-				"url='" + url + '\'' +
-				", dataList='" + dataList + '\'' +
-				", firstEpisode='" + firstEpisode + '\'' +
-				", titleOnMAL='" + titleOnMAL + '\'' +
-				", min='" + min + '\'' +
-				", max='" + max + '\'' +
-				", currentMax='" + currentMax + '\'' +
-				", posterUrl='" + posterUrl + '\'' +
-				", finalUrl='" + finalUrl + '\'' +
-				", numberOfEpisodeForWatch='" + numberOfEpisodeForWatch + '\'' +
-				'}';
 	}
 }
--- src/main/java/nasirov/yv/serialization/AnimediaTitleSearchInfo.java
+++ src/main/java/nasirov/yv/serialization/AnimediaTitleSearchInfo.java
@@ -40,66 +40,13 @@ public class AnimediaTitleSearchInfo {
 	@Getter
 	@Setter
 	private String posterUrl;
-//
-//	public String getTitle() {
-//		return title;
-//	}
-//
+
 	public void setTitle(String title) {
 		this.title = title.toLowerCase();
 	}
-//
-//	public String getKeywords() {
-//		return keywords;
-//	}
-//
+	
 	public void setKeywords(String keywords) {
 		this.keywords = keywords.toLowerCase();
 	}
-//
-//	public String getUrl() {
-//		return url;
-//	}
-//
-//	public void setUrl(String url) {
-//		this.url = url;
-//	}
-//
-//	public String getPosterUrl() {
-//		return posterUrl;
-//	}
-//
-//	public void setPosterUrl(String posterUrl) {
-//		this.posterUrl = posterUrl;
-//	}
-//
-//	@Override
-//	public boolean equals(Object o) {
-//		if (this == o) return true;
-//		if (o == null || getClass() != o.getClass()) return false;
-//		AnimediaTitleSearchInfo that = (AnimediaTitleSearchInfo) o;
-//		if (!title.equals(that.title)) return false;
-//		if (!keywords.equals(that.keywords)) return false;
-//		if (!url.equals(that.url)) return false;
-//		return posterUrl.equals(that.posterUrl);
-//	}
-//
-//	@Override
-//	public int hashCode() {
-//		int result = title.hashCode();
-//		result = 31 * result + keywords.hashCode();
-//		result = 31 * result + url.hashCode();
-//		result = 31 * result + posterUrl.hashCode();
-//		return result;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "AnimediaTitleSearchInfo{" +
-//				"title='" + title + '\'' +
-//				", keywords='" + keywords + '\'' +
-//				", url='" + url + '\'' +
-//				", posterUrl='" + posterUrl + '\'' +
-//				'}';
-//	}
+
 }
