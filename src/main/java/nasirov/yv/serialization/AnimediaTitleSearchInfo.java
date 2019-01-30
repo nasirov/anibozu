9ecaa82e88e1e51b2eef664154c94cd124a18660
hikka <yurqaa@mail.ru>
2019-01-30 12:00:44 +0300

    refactor AnimediaTitleSearchInfo

--- src/main/java/nasirov/yv/serialization/AnimediaTitleSearchInfo.java
+++ src/main/java/nasirov/yv/serialization/AnimediaTitleSearchInfo.java
@@ -1,5 +1,6 @@
 package nasirov.yv.serialization;
 
+import lombok.*;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonProperty;
 
@@ -8,87 +9,97 @@ import org.codehaus.jackson.annotate.JsonProperty;
  * Created by Хикка on 17.12.2018.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class AnimediaTitleSearchInfo {
 	/**
 	 * Anime title
 	 */
+	@Getter
 	private String title;
 	
 	/**
 	 * Keywords for search
 	 */
+	@Getter
 	private String keywords;
 	
 	/**
 	 * Anime URL
 	 */
+	@Getter
+	@Setter
 	private String url;
 	
 	/**
 	 * Poster URL
 	 */
 	@JsonProperty(value = "poster")
+	@Getter
+	@Setter
 	private String posterUrl;
-	
-	public String getTitle() {
-		return title;
-	}
-	
+//
+//	public String getTitle() {
+//		return title;
+//	}
+//
 	public void setTitle(String title) {
 		this.title = title.toLowerCase();
 	}
-	
-	public String getKeywords() {
-		return keywords;
-	}
-	
+//
+//	public String getKeywords() {
+//		return keywords;
+//	}
+//
 	public void setKeywords(String keywords) {
 		this.keywords = keywords.toLowerCase();
 	}
-	
-	public String getUrl() {
-		return url;
-	}
-	
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	
-	public String getPosterUrl() {
-		return posterUrl;
-	}
-	
-	public void setPosterUrl(String posterUrl) {
-		this.posterUrl = posterUrl;
-	}
-	
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		AnimediaTitleSearchInfo that = (AnimediaTitleSearchInfo) o;
-		if (!title.equals(that.title)) return false;
-		if (!keywords.equals(that.keywords)) return false;
-		if (!url.equals(that.url)) return false;
-		return posterUrl.equals(that.posterUrl);
-	}
-	
-	@Override
-	public int hashCode() {
-		int result = title.hashCode();
-		result = 31 * result + keywords.hashCode();
-		result = 31 * result + url.hashCode();
-		result = 31 * result + posterUrl.hashCode();
-		return result;
-	}
-	
-	@Override
-	public String toString() {
-		return "AnimediaTitleSearchInfo{" +
-				"title='" + title + '\'' +
-				", keywords='" + keywords + '\'' +
-				", url='" + url + '\'' +
-				", posterUrl='" + posterUrl + '\'' +
-				'}';
-	}
+//
+//	public String getUrl() {
+//		return url;
+//	}
+//
+//	public void setUrl(String url) {
+//		this.url = url;
+//	}
+//
+//	public String getPosterUrl() {
+//		return posterUrl;
+//	}
+//
+//	public void setPosterUrl(String posterUrl) {
+//		this.posterUrl = posterUrl;
+//	}
+//
+//	@Override
+//	public boolean equals(Object o) {
+//		if (this == o) return true;
+//		if (o == null || getClass() != o.getClass()) return false;
+//		AnimediaTitleSearchInfo that = (AnimediaTitleSearchInfo) o;
+//		if (!title.equals(that.title)) return false;
+//		if (!keywords.equals(that.keywords)) return false;
+//		if (!url.equals(that.url)) return false;
+//		return posterUrl.equals(that.posterUrl);
+//	}
+//
+//	@Override
+//	public int hashCode() {
+//		int result = title.hashCode();
+//		result = 31 * result + keywords.hashCode();
+//		result = 31 * result + url.hashCode();
+//		result = 31 * result + posterUrl.hashCode();
+//		return result;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "AnimediaTitleSearchInfo{" +
+//				"title='" + title + '\'' +
+//				", keywords='" + keywords + '\'' +
+//				", url='" + url + '\'' +
+//				", posterUrl='" + posterUrl + '\'' +
+//				'}';
+//	}
 }
