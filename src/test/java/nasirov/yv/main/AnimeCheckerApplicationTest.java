d057567c4ac8f1b9c1c483789454454fb1ba1d8b
hikka <yurqaa@mail.ru>
2019-05-14 06:52:50 +0300

    add test on main

---
+++ src/test/java/nasirov/yv/main/AnimeCheckerApplicationTest.java
@@ -0,0 +1,22 @@
+package nasirov.yv.main;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.stream.Stream;
+import nasirov.yv.AbstractTest;
+import nasirov.yv.AnimeCheckerApplication;
+import org.junit.Test;
+
+/**
+ * Created by nasirov.yv
+ */
+public class AnimeCheckerApplicationTest extends AbstractTest {
+
+	@Test
+	public void testMain() throws Exception {
+		AnimeCheckerApplication.main(new String[0]);
+		assertEquals(1,
+				Stream.of(Thread.getAllStackTraces()).flatMap(x -> x.entrySet().stream()).filter(x -> x.getKey().getName().matches("main")).count());
+	}
+
+}
