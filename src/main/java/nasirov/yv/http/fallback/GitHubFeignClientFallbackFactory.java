2de089cf8f98d3f8fbae06053c04f19103a0236e
hikka <yurqaa@mail.ru>
2020-02-02 09:22:06 +0300

    add GitHubFeignClientFallbackFactory

---
+++ src/main/java/nasirov/yv/http/fallback/GitHubFeignClientFallbackFactory.java
@@ -0,0 +1,21 @@
+package nasirov.yv.http.fallback;
+
+import feign.hystrix.FallbackFactory;
+import java.util.Collections;
+import lombok.extern.slf4j.Slf4j;
+import nasirov.yv.http.feign.GitHubFeignClient;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class GitHubFeignClientFallbackFactory implements FallbackFactory<GitHubFeignClient> {
+
+	@Override
+	public GitHubFeignClient create(Throwable cause) {
+		return x -> {
+			log.error("GitHubFeignClient fallback during call /nasirov/anime-checker-resources/master/references.json | Cause message [{}]",
+					cause.getMessage());
+			return Collections.emptySet();
+		};
+	}
+}
--- src/main/java/nasirov/yv/http/feign/GitHubFeignClient.java
+++ src/main/java/nasirov/yv/http/feign/GitHubFeignClient.java
@@ -5,6 +5,7 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import java.util.Set;
 import nasirov.yv.data.animedia.TitleReference;
 import nasirov.yv.http.config.FeignClientConfig;
+import nasirov.yv.http.fallback.GitHubFeignClientFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Created by nasirov.yv
  */
-@FeignClient(value = "github-feign-client", configuration = FeignClientConfig.class)
+@FeignClient(value = "github-feign-client", configuration = FeignClientConfig.class, fallbackFactory = GitHubFeignClientFallbackFactory.class)
 @RequestMapping(value = "/nasirov/anime-checker-resources/master", headers = {"Accept-Encoding=gzip, deflate, br"})
 public interface GitHubFeignClient {
 
