33c438b67ba2d1cd77a205e2e78c9250328de404
hikka <yurqaa@mail.ru>
2020-02-11 21:35:30 +0300

    format properties beans

--- src/main/java/nasirov/yv/data/properties/ServicesAvailability.java
+++ src/main/java/nasirov/yv/data/properties/ServicesAvailability.java
@@ -6,6 +6,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
+/**
+ * Created by nasirov.yv
+ */
 @Data
 @Validated
 @Configuration
