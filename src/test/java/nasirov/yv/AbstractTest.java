df5f38cf8d93fb3d12a2aba494f1eb2d21895f5f
hikka <yurqaa@mail.ru>
2020-11-14 13:29:17 +0300

    update pom

--- src/test/java/nasirov/yv/AbstractTest.java
+++ src/test/java/nasirov/yv/AbstractTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
@@ -19,7 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
 /**
  * Created by nasirov.yv
  */
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureCache
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
