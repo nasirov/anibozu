ddec2e7d8e7c502d09f5d97aaae38ae3a2424cd9
yasamuro <yurqaa@mail.ru>
2023-02-13 04:23:08 +0300

    add cors to ProcessController

--- src/main/java/nasirov/yv/ab/controller/ProcessController.java
+++ src/main/java/nasirov/yv/ab/controller/ProcessController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import nasirov.yv.ab.dto.fe.ProcessResult;
 import nasirov.yv.ab.service.ProcessServiceI;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import reactor.core.publisher.Mono;
  * @author Nasirov Yuriy
  */
 @Validated
+@CrossOrigin(origins = "https://anibozu.nasirov.info")
 @RestController
 @RequiredArgsConstructor
 public class ProcessController {
