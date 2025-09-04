976d33872f97254bbdfaadedb53755f06a29c80b
nasirov <47319902+nasirov@users.noreply.github.com>
2025-09-05 01:29:10 +0400

    migrate form slider to modal for showing anime site links

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,10 +1,12 @@
 const THEMES = {DARK: 'dark', LIGHT: 'light'};
 const THEME_ATTR = 'theme';
 const THEME_COOKIE = 'theme';
-const DIRECTION = {LEFT: 'left', RIGHT: 'right'};
 const GENERIC_ERROR_MESSAGE = 'Sorry, something went wrong. Please try again later.';
 const TOO_MANY_REQUESTS_ERROR_MESSAGE = 'You\'ve performed too many requests. Please try again later.';
 const SVG_NS = 'http://www.w3.org/2000/svg';
+const CLICK_TYPE = 'click';
+const KEYPRESS_EVENT_TYPE = 'keypress';
+const KEY_ENTER = 'Enter';
 
 document.addEventListener('DOMContentLoaded', () => {
 	switchTheme();
@@ -30,20 +32,19 @@ function switchTheme() {
 
 function setThemeSwitcherEvents() {
 	const themeSwitcher = document.querySelector('.theme-switcher');
-	const clickType = 'click';
-	const keypressEventType = 'keypress';
-	[clickType, keypressEventType].forEach(type => {
-				themeSwitcher.addEventListener(type, function (e) {
-					if (e.type === clickType || (e.type === keypressEventType && e.key === 'Enter')) {
-						const currentTheme = document.documentElement.getAttribute(THEME_ATTR);
-						const nextTheme = currentTheme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
-						setTheme(nextTheme);
-						if (window.localStorage) {
-							localStorage.setItem(THEME_COOKIE, nextTheme);
-						}
+
+	[CLICK_TYPE, KEYPRESS_EVENT_TYPE].forEach(type => {
+			themeSwitcher.addEventListener(type, function (e) {
+				if (e.type === CLICK_TYPE || (e.type === KEYPRESS_EVENT_TYPE && e.key === KEY_ENTER)) {
+					const currentTheme = document.documentElement.getAttribute(THEME_ATTR);
+					const nextTheme = currentTheme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
+					setTheme(nextTheme);
+					if (window.localStorage) {
+						localStorage.setItem(THEME_COOKIE, nextTheme);
 					}
-				});
-			}
+				}
+			});
+		}
 	);
 }
 
@@ -55,22 +56,22 @@ function setTheme(theme) {
 
 function setFormEvents() {
 	document.querySelector('form').addEventListener('submit',
-			async e => {
-				e.preventDefault();
-				const input = document.querySelector('input:valid');
-				if (input) {
-					const username = input.value;
-					input.setAttribute('readonly', '');
-					const button = document.querySelector('button');
-					const icon = button.querySelector('svg');
-					const intervalId = setLoadingProgress(button);
-					await renderAnimeList(username);
-					input.removeAttribute('readonly');
-					clearInterval(intervalId);
-					setText(button, '');
-					button.appendChild(icon);
-				}
-			});
+		async e => {
+			e.preventDefault();
+			const input = document.querySelector('input:valid');
+			if (input) {
+				const username = input.value;
+				input.setAttribute('readonly', '');
+				const button = document.querySelector('button');
+				const icon = button.querySelector('svg');
+				const intervalId = setLoadingProgress(button);
+				await renderAnimeList(username);
+				input.removeAttribute('readonly');
+				clearInterval(intervalId);
+				setText(button, '');
+				button.appendChild(icon);
+			}
+		});
 }
 
 function setLoadingProgress(element) {
@@ -117,14 +118,13 @@ async function renderAnimeList(username) {
 			item.appendChild(itemBadges);
 			const episodes = anime.episodes;
 			if (episodes.length > 0) {
-				const animeSiteBadgesList = buildAnimeSiteBadgesList(episodes);
-				itemBadges.append(...animeSiteBadgesList);
-				const slider = buildSlider(episodes[0]);
-				if (episodes.length > 1) {
-					slider.prepend(buildSliderArrow(DIRECTION.LEFT));
-					slider.appendChild(buildSliderArrow(DIRECTION.RIGHT));
+				const stats = buildDiv('anime_stats_badges');
+				addTranslationTypeStats(episodes, 'sub', stats);
+				addTranslationTypeStats(episodes, 'dub', stats);
+				if (stats.childNodes.length > 0) {
+					itemBadges.appendChild(stats);
 				}
-				item.appendChild(slider);
+				item.appendChild(buildDialog(item, episodes));
 			}
 			items.appendChild(item);
 		}
@@ -144,8 +144,8 @@ function buildDiv(targetClass) {
 }
 
 function buildItem() {
-	const result = buildDiv('item');
-	result.setAttribute('data-id', '1');
+	const result = buildDiv('item navigable');
+	result.setAttribute('tabindex', '0');
 	return result;
 }
 
@@ -161,7 +161,10 @@ function buildPoster(anime) {
 }
 
 function buildMalBadges(anime) {
-	const result = buildDiv('mal_badges');
+	const result = buildLink('mal_badges navigable', anime.malUrl);
+	if (anime.name) {
+		result.appendChild(buildBadge(anime.name));
+	}
 	result.appendChild(buildMalEpisodeBadge(anime));
 	if (anime.airing) {
 		result.appendChild(buildBadge('Airing'));
@@ -170,38 +173,11 @@ function buildMalBadges(anime) {
 }
 
 function buildMalEpisodeBadge(anime) {
-	const result = buildLink('badge navigable', anime.malUrl);
+	const result = buildDiv('badge navigable');
 	const maxEpisodes = anime.maxEpisodes;
-	const title = 'Ep. ' + anime.nextEpisode + ' / ' + (maxEpisodes === 0 ? '?' : maxEpisodes);
+	const title = 'Next ep. ' + anime.nextEpisode + ' / ' + (maxEpisodes === 0 ? '?' : maxEpisodes);
 	result.setAttribute('title', title);
 	setText(result, title);
-	return result;
-}
-
-function buildSlider(episode) {
-	const result = buildDiv('slider');
-	result.appendChild(buildAnimeLink(episode));
-	return result;
-}
-
-function buildSliderArrow(direction) {
-	const result = buildArrowSvg(direction);
-	const clickType = 'click';
-	const keypressEventType = 'keypress';
-	[clickType, keypressEventType].forEach(type => {
-				result.addEventListener(type, function (e) {
-					if (e.type === clickType || (e.type === keypressEventType && e.key === 'Enter')) {
-						const item = this.parentNode.parentNode;
-						const currentDataId = item.getAttribute('data-id');
-						const nextDataId = getNextDataId(direction, currentDataId, item);
-						toggleAnimeSiteBadgesActive(item, currentDataId);
-						item.setAttribute('data-id', nextDataId);
-						toggleAnimeSiteBadgesActive(item, nextDataId);
-						setNextAnimeLink(item, nextDataId);
-					}
-				});
-			}
-	);
 	return result;
 }
 
@@ -215,88 +191,91 @@ function buildSvg(targetClass, viewBox, dValue) {
 	return result;
 }
 
-function buildArrowSvg(direction) {
-	let dValue;
-	if (DIRECTION.LEFT === direction) {
-		dValue = 'M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z';
-	} else {
-		dValue = 'M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z';
-	}
-	const result = buildSvg('slider__arrow slider__arrow--' + direction, '0 0 320 512', dValue);
-	result.setAttribute('tabindex', '0');
-	return result;
-}
+function addTranslationTypeStats(episodes, translationType, stats) {
+	const matchedAmount = episodes.filter(x => x.extra.includes(translationType)).length;
+	if (matchedAmount > 0) {
+		let badge;
 
-function buildPlayIconSvg() {
-	const dValue = 'M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80L0 432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z';
-	return buildSvg('play_icon', '0 0 384 512', dValue);
-}
-
-function getNextDataId(direction, currentId, item) {
-	const firstId = 1;
-	const lastId = item.querySelectorAll('div[id]').length;
-	let result;
-	currentId = Number.parseInt(currentId);
-	if (DIRECTION.LEFT === direction) {
-		result = currentId - 1;
-		if (result < firstId) {
-			result = lastId;
+		if (translationType === 'dub') {
+			badge = buildDubBadge();
 		}
-	} else {
-		result = currentId + 1;
-		if (result > lastId) {
-			result = firstId;
+
+		if (translationType === 'sub') {
+			badge = buildSubBadge();
+		}
+
+		if (badge) {
+			const span = document.createElement('span');
+			setText(span, matchedAmount);
+			badge.appendChild(span);
+			stats.appendChild(badge);
 		}
 	}
+}
+
+function buildDialog(item, episodes) {
+	const result = document.createElement('dialog');
+	const closeButton = buildDialogCloseButton();
+
+	[CLICK_TYPE, KEYPRESS_EVENT_TYPE].forEach(type => {
+		item.addEventListener(type, function (e) {
+			if (e.target === item && e.key === KEY_ENTER) {
+				result.showModal();
+			}
+
+			if (e.type !== CLICK_TYPE) {
+				return;
+			}
+
+			if (e.target === item ||
+				e.target.className === 'item__badges' ||
+				e.target.className === 'anime_stats_badges' ||
+				e.target.parentElement.className === 'badge' ||
+				e.target.parentElement.className === 'anime_stats_badges') {
+				result.showModal();
+			}
+		});
+
+		closeButton.addEventListener(type, e => {
+			if (e.type === CLICK_TYPE || e.key === KEY_ENTER) {
+				result.close();
+			}
+		});
+		}
+	);
+
+	result.addEventListener(CLICK_TYPE, e => {
+		if (e.target === result) {
+			result.close();
+		}
+	});
+
+	const animeSiteBadgesList = buildAnimeSiteBadgesList(episodes);
+	result.append(...animeSiteBadgesList);
+	result.appendChild(closeButton);
 	return result;
-}
-
-function toggleAnimeSiteBadgesActive(item, id) {
-	item.querySelector('div[id="' + id + '"]').classList.toggle('anime_site_badges--active');
-}
-
-function setNextAnimeLink(item, dataId) {
-	const nextAnimeLink = item.querySelector('div[id="' + dataId + '"]').getAttribute('anime-link');
-	item.querySelector('div.slider >a').setAttribute('href', nextAnimeLink);
 }
 
 function buildAnimeSiteBadgesList(episodes) {
 	const result = [];
 	for (const i in episodes) {
 		const episode = episodes[i];
-		const id = Number.parseInt(i) + 1;
-		const badges = buildAnimeSiteBadges(episode, id);
-		badges.appendChild(buildBadge(episode.name));
-		badges.appendChild(buildBadge('Link ' + id + ' / ' + episodes.length));
-		if (i === '0') {
-			badges.classList.toggle('anime_site_badges--active');
+		const badges = buildLink('anime_site_badges navigable', episode.link);
+
+		for (const value of episode.extra) {
+			if (value === 'dub') {
+				badges.appendChild(buildDubBadge());
+			} else if (value === 'sub') {
+				badges.appendChild(buildSubBadge());
+			} else {
+				badges.appendChild(buildBadge(value));
+			}
 		}
+
+		badges.appendChild(buildBadge(episode.name));
 		result.push(badges);
 	}
-	return result;
-}
 
-function buildAnimeSiteBadges(episode, id) {
-	const result = buildDiv('anime_site_badges');
-	result.setAttribute('id', id);
-	result.setAttribute('anime-link', episode.link);
-
-	const translationList = [];
-	for (const value of episode.extra) {
-		if (value === 'dub') {
-			translationList.push(buildDubBadge(value));
-		} else if (value === 'sub') {
-			translationList.push(buildSubBadge(value));
-		} else {
-			result.appendChild(buildBadge(value));
-		}
-	}
-
-	if (translationList.length > 0) {
-		const translations = buildDiv('translations');
-		translations.append(...translationList);
-		result.appendChild(translations);
-	}
 	return result;
 }
 
@@ -312,24 +291,25 @@ function buildBadgeWithoutText(title) {
 	return result;
 }
 
-function buildDubBadge(value) {
-	const result = buildBadgeWithoutText(value);
+function buildDubBadge() {
+	const result = buildBadgeWithoutText('dub');
 	const dValue = 'M192 0C139 0 96 43 96 96l0 160c0 53 43 96 96 96s96-43 96-96l0-160c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40c0 89.1 66.2 162.7 152 174.4l0 33.6-48 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l72 0 72 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-48 0 0-33.6c85.8-11.7 152-85.3 152-174.4l0-40c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40c0 70.7-57.3 128-128 128s-128-57.3-128-128l0-40z';
 	result.appendChild(buildSvg('', '0 0 384 512', dValue));
 	return result;
 }
 
-function buildSubBadge(value) {
-	const result = buildBadgeWithoutText(value);
+function buildSubBadge() {
+	const result = buildBadgeWithoutText('sub');
 	const dValue = 'M0 96C0 60.7 28.7 32 64 32l448 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zM200 208c14.2 0 27 6.1 35.8 16c8.8 9.9 24 10.7 33.9 1.9s10.7-24 1.9-33.9c-17.5-19.6-43.1-32-71.5-32c-53 0-96 43-96 96s43 96 96 96c28.4 0 54-12.4 71.5-32c8.8-9.9 8-25-1.9-33.9s-25-8-33.9 1.9c-8.8 9.9-21.6 16-35.8 16c-26.5 0-48-21.5-48-48s21.5-48 48-48zm144 48c0-26.5 21.5-48 48-48c14.2 0 27 6.1 35.8 16c8.8 9.9 24 10.7 33.9 1.9s10.7-24 1.9-33.9c-17.5-19.6-43.1-32-71.5-32c-53 0-96 43-96 96s43 96 96 96c28.4 0 54-12.4 71.5-32c8.8-9.9 8-25-1.9-33.9s-25-8-33.9 1.9c-8.8 9.9-21.6 16-35.8 16c-26.5 0-48-21.5-48-48z';
 	result.appendChild(buildSvg('', '0 0 576 512', dValue));
 	return result;
 }
 
-function buildAnimeLink(episode) {
-	const result = buildLink('anime_link', episode.link);
+function buildDialogCloseButton() {
+	const result = buildDiv('dialog__close-button navigable');
 	result.setAttribute('tabindex', '0');
-	result.appendChild(buildPlayIconSvg());
+	const dValue = 'M183.1 137.4C170.6 124.9 150.3 124.9 137.8 137.4C125.3 149.9 125.3 170.2 137.8 182.7L275.2 320L137.9 457.4C125.4 469.9 125.4 490.2 137.9 502.7C150.4 515.2 170.7 515.2 183.2 502.7L320.5 365.3L457.9 502.6C470.4 515.1 490.7 515.1 503.2 502.6C515.7 490.1 515.7 469.8 503.2 457.3L365.8 320L503.1 182.6C515.6 170.1 515.6 149.8 503.1 137.3C490.6 124.8 470.3 124.8 457.8 137.3L320.5 274.7L183.1 137.4z';
+	result.appendChild(buildSvg('icon', '0 0 640 640', dValue));
 	return result;
 }
 
